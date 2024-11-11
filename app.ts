4930817240064a04fcaf71aa378b6f0629d5e73d
oJaciel <jacielhbarichellO@gmail.com>
2024-11-11 20:47:34 -0300

    Rota p/ POST (Create)
    Definida a rota para o POST de student, com teste incluso no arquivo de rotas

--- app.ts
+++ app.ts
@@ -1,14 +1,16 @@
-import express from 'express'
+import bodyParser from 'body-parser'
+import express from 'express';
 import { PrismaClient } from '@prisma/client';
-
-
+import { randomUUID } from "crypto";
 //Implementando o Prisma
 const prisma = new PrismaClient()
 
 const app = express()
 const port = 3000 //Aqui define em qual porta vai rodar o app
 
-app.use(express.json())
+app.use(express.json());
+//Usando o bodyParser para receber o body da request
+app.use(bodyParser.json());
 
 const baseUrl = '/api/students'
 
@@ -19,8 +21,18 @@ app.get(`${baseUrl}`, async (req, res) => {
 })
 
 //Rota para o post (create) do alunos
-app.post(`${baseUrl}`, (req, res) => {
-  res.send('Aqui é o post dos alunos')
+app.post(`${baseUrl}`, async (req, res) => {
+  const student = await prisma.students.create({
+    data: {
+      id: randomUUID(),
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age
+      //O req.body é para captar o corpo da request
+    }
+  })
+  
+  res.json(student)
 })
 
 //Rota para o put (update) dos alunos
