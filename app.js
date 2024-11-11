3813dfd741cb314099cba8a6e284ddaae7ac0f5f
oJaciel <jacielhbarichellO@gmail.com>
2024-11-10 21:43:04 -0300

    Criação Rotas Base
    Criando as rotas base e testando elas com o REST Client

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Worlde!')
+const app = express()
+const port = 3000 //Aqui define em qual porta vai rodar o app
+
+const baseUrl = '/api/students'
+
+//Rota para o get (read) dos alunos
+app.get(`${baseUrl}`, (req, res) => {
+  res.send('Aqui é o get dos alunos')
 })
+
+//Rota para o post (create) do alunos
+app.post(`${baseUrl}`, (req, res) => {
+  res.send('Aqui é o post dos alunos')
+})
+
+//Rota para o put (update) dos alunos
+app.put(`${baseUrl}/:id`, (req, res) => {
+  res.send('Aqui é o put dos alunos')
+})
+
+//Rota para o delete dos alunos
+app.delete(`${baseUrl}/:id`, (req, res) => {
+  res.send('Aqui é o delete dos alunos')
+})
+
+app.listen(port);
+//localhost:3000
